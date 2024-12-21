f499ae58790f737bf30760ebc00e2de62df0c3d2
Hermann Rodriguez <hermann.rodriguez@ikitech.com.co>
2024-12-20 20:03:19 -0500

    Initial commit

---
+++ src/main/java/com/hero/medconsult/back/MedconsultBackApplication.java
@@ -0,0 +1,13 @@
+package com.hero.medconsult.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedconsultBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedconsultBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hero/medconsult/back/MedconsultBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.medconsult.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedconsultBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
